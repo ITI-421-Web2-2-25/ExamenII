afe45413e4a81bf4581e3172e3bcb9486e807a02
KevinPicado27 <kpicari@gmail.com>
2025-08-22 17:40:35 -0600

    change database connection

--- backend/src/config/dbContext.js
+++ backend/src/config/dbContext.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const contextDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/northwind_kgpa", { });
+    await mongoose.connect("mongodb://localhost:27017/examen_kevinpicado_frankmora", { });
     console.log("Conexión base de datos exitosa...");
   } catch (err) {
     console.error("Error no se puede conectar con la base de datos:", err.message);
